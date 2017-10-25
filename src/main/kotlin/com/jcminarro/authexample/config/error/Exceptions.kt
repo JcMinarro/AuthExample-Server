b8f6ae818e4ac18ecc1a96fc43187cf235d406aa
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-25 21:22:15 +0200

    Create an Exception to notify that the refreshToken is not valid

--- src/main/kotlin/com/jcminarro/authexample/config/error/Exceptions.kt
+++ src/main/kotlin/com/jcminarro/authexample/config/error/Exceptions.kt
@@ -5,3 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 class InvalidCredentialException: RuntimeException()
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+class InvalidRefreshTokenException: RuntimeException()
