c46be695755233f618f1b90325c82b312730c323
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-25 17:53:18 +0200

    Create User and OAuth models

---
+++ src/main/kotlin/com/jcminarro/authexample/model/Models.kt
@@ -0,0 +1,7 @@
+package com.jcminarro.authexample.model
+
+import com.google.gson.annotations.SerializedName
+
+data class User(val id: Int, val username: String, val password: String)
+data class OAuth(@SerializedName("accessToken") val accessToken: String,
+                 @SerializedName("refreshToken") val refreshToken: String)
