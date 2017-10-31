8bb50314b0be1aa1c62fd27480c82d761a583c4e
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-10-31 17:44:27 +0100

    Reduce time that AccessToken is valid to 30 Seconds

--- src/main/kotlin/com/jcminarro/authexample/db/AccessTokenDB.kt
+++ src/main/kotlin/com/jcminarro/authexample/db/AccessTokenDB.kt
@@ -3,4 +3,4 @@ package com.jcminarro.authexample.db
 import org.springframework.stereotype.Component
 
 @Component
-class AccessTokenDB: InMemoryCacheMap<Int>(5 * 60 * 1000)
+class AccessTokenDB: InMemoryCacheMap<Int>(30 * 1000)
